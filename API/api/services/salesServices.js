e383e55a68301992863d71d355e8049714a0340b
Jeferson Marin <stiv.marin@utp.edu.co>
2024-01-05 01:40:31 -0500

    Sistema de ventas actualizado - Acceder a imagenes del menú

--- API/api/services/salesServices.js
+++ API/api/services/salesServices.js
@@ -1,41 +1,44 @@
-const boom=require('@hapi/boom');
-//const getConnection = require('../../libs/postgress'); NO ESTOY HACIENDO SELECTS, POR TANTO NO SE USARÁ
+const boom = require('@hapi/boom');
+const { models } = require('../../libs/sequelize');
 
-const {models}= require('../../libs/sequelize')
+class SalesService {
+  constructor() {}
 
-class SalesService
-{
-    constructor(){}
-    async create(data)
-    {
-      const newSale = await models.Sales.create(data);
-      return newSale;
-    }
-    async find(){
-      const rta= await models.Sales.findAll({
-        include: ['menu'],
-      });
-      return rta;
+  async create(data) {
+    const newSale = await models.Sales.create(data);
+    return newSale;
   }
-    async findOne(id)
-    {
-      const sale = await models.Sales.findByPk(id);
-      if(!sale){
-        throw boom.notFound('sale not found')
-      }
-      return sale;
-    }
-    async update(id,changes)
-    {
-      const sale = await this.findOne(id);
-      const rta = await sale.update(changes);
-      return rta;
-    }
-    async delete(id)
-    {
-      const sale = await this.findOne(id);
-      await sale.destroy();
-      return {id};
-    }
+
+  async find() {
+    const rta = await models.Sales.findAll({
+      include: [{ model: models.Menu, as: 'menu', include: ['images'] }],
+    });
+    return rta;
   }
-module.exports=SalesService;
+
+  async findOne(id) {
+    const sale = await models.Sales.findByPk(id, {
+      include: [{ model: models.Menu, as: 'menu', include: ['images'] }],
+    });
+
+    if (!sale) {
+      throw boom.notFound('Sale not found');
+    }
+
+    return sale;
+  }
+
+  async update(id, changes) {
+    const sale = await this.findOne(id);
+    const rta = await sale.update(changes);
+    return rta;
+  }
+
+  async delete(id) {
+    const sale = await this.findOne(id);
+    await sale.destroy();
+    return { id };
+  }
+}
+
+module.exports = SalesService;
